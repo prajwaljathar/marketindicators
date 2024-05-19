5573d7888adef417897ae27c5e2e8d5e8f3dae9a
admin <prajwaljathar1999@gmail.com>
2024-05-19 17:31:49 +0530

    Signed-off-by: admin <prajwaljathar1999@gmail.com>

--- demo/src/main/java/com/example/demo/contoller/SurveillanceController.java
+++ demo/src/main/java/com/example/demo/contoller/SurveillanceController.java
@@ -3,6 +3,7 @@ package com.example.demo.contoller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class SurveillanceController {
     @Autowired
     private SurveillanceDataRepository surveillanceDataRepository;
 
-    @PostMapping("/surveillance/upload")
+    @GetMapping("/surveillance/upload")
     public ResponseEntity<String> uploadSurveillanceData(@RequestParam("type") String type) {
         try {
             // Construct the complete file path
